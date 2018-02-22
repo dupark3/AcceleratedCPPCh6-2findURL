a3224e758eb6abd2a40d8f97b689a708f4932d4e
Du Park <dupark3@gmail.com>
2018-02-22 14:46:05 -0500

    Find_urls function outline

---
+++ find_urls.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+typedef std::string::const_iterator iter;
+
+std::vector<std::string> find_urls(const std::string& input){
+    std::vector<std::string> ret;
+    iter beg = input.begin();
+    iter end = intput.end();
+
+    // Look through entier input
+    while (beg != end){
+        // Look for one or more letters followed by ://
+        beg = url_beg(beg, end);
+
+        // if we found it
+        if (beg != end){
+            // get the rest of the url
+            iter after = url_end(beg, end);
+
+            // remember the url
+            ret.push_back(std::string(beg, after));
+
+            // advance beg and check for more urls on this line
+            beg = after;
+        }
+    }
+
+    return ret;
+}
---
+++ find_urls.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_find_urls_h
+#define GUARD_find_urls_h
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> find_urls(const std::string&);
+
+#endif // GUARD_find_urls_h
