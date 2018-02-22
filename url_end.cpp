147d404be7e0a99430537b4a5e4a54e0058fb925
Du Park <dupark3@gmail.com>
2018-02-22 14:53:15 -0500

    url_end function added

---
+++ url_end.cpp
@@ -0,0 +1,16 @@
+#include <algorithm> //find, find_if
+#include <string>
+
+
+bool not_url_char(char c){
+    // characters, in addition to alphanumerics, that can appear in a url
+    static const std::string url_ch = "~;/?:@=&$-_.+!*'(),";
+
+    // see whether c can appear in a url and return the opposite
+    return !(isalnum(c) ||
+             std::find(url_ch.begin(), url_ch.end(), c) != url_ch.end());
+}
+
+std::string::const_iterator url_end (std::string::const_iterator beg, std::string::const_iterator end){
+    return std::find_if(beg, end, not_url_char);
+}
---
+++ url_end.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_url_end_h
+#define GUARD_url_end_h
+
+#include <string>
+
+bool not_url_char(char);
+
+std::string::const_iterator url_end (std::string::const_iterator, std::string::const_iterator);
+
+#endif // GUARD_url_end_h
